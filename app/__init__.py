1ede1bfb5a05ffcca2c0a43a0495337fb781046b
EDUARDO MARQUES DOS SANTOS <60169862089@senacrs.edu.br>
2026-06-25 19:26:25 -0300

    finalização

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from flask import Flask
 from dotenv import load_dotenv
-from app.extensions import db
-from app.routes import web_bp
+from app.extensions import db, cors
+from app.routes import web_bp, api_bp
 
 load_dotenv()
 
@@ -11,11 +11,22 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
-    
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
     db.init_app(app)
 
+    origens_permitidas = os.getenv("CORS_ORIGINS").split(",")
+
+    cors.init_app(app, resources={
+        r"/api/*":
+            {
+                "origins": origens_permitidas
+            }
+    })
+
     app.register_blueprint(web_bp)
+    app.register_blueprint(api_bp)
 
     with app.app_context():
         db.create_all()
